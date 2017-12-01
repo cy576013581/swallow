8fd9d3311edc059294df2ec0ac8d017a008fba9a
cy576013581 <pjchenyang@qq.com>
2017-12-01 16:25:39 +0800

    STORY24: 使用定时器，定时向管理员汇报系统情况(具体发送内容需要完善)

--- src/main/java/com/cy/example/Application.java
+++ src/main/java/com/cy/example/Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling   //开启定时任务
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/cy/example/supplement/scheduler/ReportScheduler.java
@@ -0,0 +1,29 @@
+package com.cy.example.supplement.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.cy.example.entity.MailEntity;
+import com.cy.example.supplement.rabbitmq.general.RabbitSender;
+import com.cy.example.util.DateUtil;
+
+public class ReportScheduler {
+	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private RabbitSender rabbitSender;
+
+	//@Scheduled(fixedRate = 6000)
+    @Scheduled(cron="0 0 24 * * ?")   //每天24点执行
+    public void reportCurrentTime() {
+    	String content = "";
+    	MailEntity mail = new MailEntity();
+		mail.setTo("pjchenyang@qq.com");
+    	mail.setContent(content);
+    	rabbitSender.sendMail(mail);
+    	logger.info("发送系统状态邮件,to" + DateUtil.getNow());
+    }
+}
