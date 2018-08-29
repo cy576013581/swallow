162125dc61bae2cb31155e7c2feb02945b0ccdca
DESKTOP-O51HBF8\chenyang <pjchenyang@qq.com>
2018-08-29 22:35:19 +0800

    恢复消息队列和redis

--- src/main/java/com/cy/example/supplement/rabbitmq/general/RabbitReceiver.java
+++ src/main/java/com/cy/example/supplement/rabbitmq/general/RabbitReceiver.java
@@ -1,5 +1,9 @@
 package com.cy.example.supplement.rabbitmq.general;
 
+import com.cy.example.util.JsonUtil;
+import com.cy.example.util.StringUtil;
+import com.cy.example.vo.IpAnalysisVo_Sina;
+import com.cy.example.vo.IpAnalysisVo_Tb;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,23 +22,58 @@ import com.cy.example.util.HttpRequestUtil;
 @Configuration
 public class RabbitReceiver {
 
-//
-//	@Autowired
-//	private ILoginRecordService loginRecordService;
-//
-//	@Autowired
-//	private IMailService mailService;
-//
-//	@RabbitListener(queues = "loginRecord")
-//    public void insertLoginRecord(LoginRecordEntity loginRecord) {
-//		log.info("接收到object : " + loginRecord);
-//		loginRecordService.insert(loginRecord);
-//    }
-//
-//	@RabbitListener(queues = "mail")
-//    public void process(MailEntity mail) {
-//		log.info("接收到object : " + mail);
-//		mailService.sendSimpleMail(mail);
-//    }
+
+	@Autowired
+	private ILoginRecordService loginRecordService;
+
+	@Autowired
+	private IMailService mailService;
+
+	@Value("${swallow.ip.analysis}")
+	private String ipAnalysis;
+
+	@RabbitListener(queues = "loginRecord")
+    public void insertLoginRecord(LoginRecordEntity loginRecord) {
+		String tbUrl = ipAnalysis.split(",")[0] + loginRecord.getC_loginIp();
+		try {
+			String addr = HttpRequestUtil.get(tbUrl);
+			IpAnalysisVo_Tb ipAnalusisTb = JsonUtil.toBean(addr, IpAnalysisVo_Tb.class);
+			if(0 == ipAnalusisTb.getCode()){
+				if(StringUtil.IsEqual("XX", ipAnalusisTb.getData().getRegion())){
+					loginRecord.setC_province(ipAnalusisTb.getData().getCity());
+					loginRecord.setC_city(ipAnalusisTb.getData().getCity());
+				}else{
+					loginRecord.setC_province(ipAnalusisTb.getData().getRegion());
+					loginRecord.setC_city(ipAnalusisTb.getData().getCity());
+				}
+
+			}else{
+				String sinaUrl = ipAnalysis.split(",")[1] + loginRecord.getC_loginIp();
+				addr = HttpRequestUtil.get(sinaUrl);
+				IpAnalysisVo_Sina ipAnalusisSina = JsonUtil.toBean(addr, IpAnalysisVo_Sina.class);
+				if(1 == ipAnalusisSina.getRet()){
+					loginRecord.setC_province(ipAnalusisSina.getProvince());
+					loginRecord.setC_city(ipAnalusisSina.getCity());
+				}else{
+					loginRecord.setC_province("未知");
+					loginRecord.setC_city("未知");
+					loginRecord.toString();
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			log.info("解析IP出错！");
+			loginRecord.setC_province("未知");
+			loginRecord.setC_city("未知");
+		}
+		log.info("接收到object : " + loginRecord);
+		loginRecordService.insert(loginRecord);
+    }
+
+	@RabbitListener(queues = "mail")
+    public void process(MailEntity mail) {
+		log.info("接收到object : " + mail);
+		mailService.sendSimpleMail(mail);
+    }
 
 }
