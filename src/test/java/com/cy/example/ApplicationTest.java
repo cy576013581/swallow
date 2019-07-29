505f05a4a626c14747b726a0867acf679b561953
DESKTOP-B44DTBV\chenyang <chenyang@gildata.com>
2019-07-29 14:18:23 +0800

    docker配置添加

--- src/test/java/com/cy/example/ApplicationTest.java
+++ src/test/java/com/cy/example/ApplicationTest.java
@@ -7,8 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.junit.Assert.*;
-
 /**
  * @description: ${description}
  * @author: chenyang
